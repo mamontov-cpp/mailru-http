67d48f740c57e58a73bdab25c8dea92775763731
Mamontov <mamontov.dp@gmail.com>
2016-02-23 16:35:36 +0300

    Oh, Ubuntu just happens to have the obsolete version of LibUV with API stuck in an early development stages. Trying to fix that.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include "cmd/daemonize.h"
 #include "server/on_connect.h"
 #include "sys/log.h"
-#include "uv.h"
+#include <uv.h>
+#include <uv-version.h>
 
 #ifndef WIN32
     #include <signal.h>
@@ -13,11 +14,18 @@ uv_tcp_t server_socket;
 cmd::Options opts;
 
 #define MAX_WRITE_HANDLES 1000
+#if UV_VERSION_MAJOR < 1
+if (status != 0) {                                                            \
+    sys::Log::write("[FATAL] %s: %s\n", msg, uv_err_name(uv_last_error()));   \
+    return 2;                                                                 \
+}
+#else
 #define CHECK(status, msg)                                           \
 if (status != 0) {                                                   \
     sys::Log::write("[FATAL] %s: %s\n", msg, uv_err_name(status));   \
     return 2;                                                        \
 }
+#endif
 
 int main(int argc, char** argv)
 {
@@ -42,7 +50,11 @@ int main(int argc, char** argv)
         struct sockaddr_in address;
         result = uv_ip4_addr(opts.IP.c_str(), static_cast<int>(opts.Port), &address);
         CHECK(result, "uv_ip4_addr");
+#if UV_VERSION_MAJOR < 1
+        result = uv_tcp_bind(&server_socket, &address, 0);
+#else
         result = uv_tcp_bind(&server_socket, reinterpret_cast<const struct sockaddr*>(&address), 0);
+#endif
         CHECK(result, "uv_tcp_bind");
         result = uv_listen(reinterpret_cast<uv_stream_t*>(&server_socket), MAX_WRITE_HANDLES, server::on_connect);
         CHECK(result, "uv_listen");
